ebe97e850e32a4fce040a75c61532bfa672c1bc1
TatyanaBogdanovich <tianabahdanovich@gmail.com>
2024-04-01 20:08:53 +0300

    Homework1

--- src/Homework1/Task1.java
+++ src/Homework1/Task1.java
@@ -2,12 +2,9 @@ package Homework1;
 
 public class Task1 {
     public static void main(String[] args) {
-
         int a = 20;
         int b = 6;
         int result = a % b;
-
         System.out.println(result);
-
     }
 }
